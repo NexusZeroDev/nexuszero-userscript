de20177a04d86a62eab5cfb7af34520ee9f4b520
Divine <divineismylife@gmail.com>
2026-02-06 19:18:51 +0800

    Change title text from 'NexGuard' to 'Nex Guard'

--- zero.user.js
+++ zero.user.js
@@ -177,7 +177,7 @@
       `;
 
       const title = document.createElement('div');
-      title.innerText = "NexGuard";
+      title.innerText = "Nex Guard";
       title.style.cssText = "color: white; font-size: 20px; font-weight: bold; margin-bottom: 10px; font-family: 'Segoe UI', sans-serif;";
 
       const countdownText = document.createElement('div');
